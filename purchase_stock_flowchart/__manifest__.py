6102e811817d72ec80b8154813f26a9e329285a7
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2024-06-01 21:34:58 +0200

    [purchase_stock_flowchart] Add purchase_stock_flowchart

---
+++ purchase_stock_flowchart/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Purchase Stock Flowchart",
+    "summary": "This module adds a flowchart to the Purchase Order to show the stock flow.",
+    "description": "This module enhances the Purchase Order by adding a flowchart that visually represents the stock flow, providing a clearer understanding of inventory movements associated with each order.",
+    'images': ['static/description/banner.png'],  # 560x280 px
+    "category": "Inventory/Purchase",
+    "version": "0.0.1",
+    "license": "AGPL-3",
+    "author": "Victor",
+    "price": 0,
+    "currency": 'EUR',
+    "depends": ["purchase_stock", "purchase", "stock", 'stock_flowchart', 'web_widget_mermaid_field'],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "installable": True,
+    'application': True,
+}
