4c5d312a589f7b524b12597747d644b52fbbf9a2
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2024-06-01 21:17:32 +0200

    [sale_stock_flowchart] Update manifest

--- sale_stock_flowchart/__manifest__.py
+++ sale_stock_flowchart/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": "This module adds a flowchart to the Sale Order to show the stock flow.",
     "description": "This module enhances the Sale Order by adding a flowchart that visually represents the stock flow, providing a clearer understanding of inventory movements associated with each order.",
     'images': ['static/description/banner.png'],  # 560x280 px
-    "category": "Technical",
+    "category": 'Sales/Sales',
     "version": "0.0.1",
     "license": "AGPL-3",
     "author": "Victor",
