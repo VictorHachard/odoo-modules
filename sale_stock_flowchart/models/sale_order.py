504d9d19813cfec41f99c07315c1847a56778ac4
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2024-06-01 21:02:19 +0200

    [sale_stock_flowchart] Addd sale_stock_flowchart

---
+++ sale_stock_flowchart/models/sale_order.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    flowchart = fields.Text('Flowchart', compute='_compute_flowchart')
+
+    @api.depends('state', 'picking_ids.state', 'picking_ids.backorder_id', 'picking_ids.backorder_ids')
+    def _compute_flowchart(self):
+        for rec in self:
+            flowchart = [
+                'graph TB',
+                rec._get_flowchart_style(),
+                rec._get_sale_picking_flowchart_graph()
+            ]
+            rec.flowchart = '\n'.join(flowchart)
+
+    def _get_sale_picking_flowchart_graph(self, more_info=False):
+        self.ensure_one()
+        flowchart = [
+            self._get_flowchart_element(more_info=more_info),
+            self._get_flowchart_click()
+        ]
+        for hierarchy in self.picking_ids._get_picking_hierarchy():
+            if len(hierarchy) > 1:
+                flowchart.append(hierarchy[0]._get_flowchart_sub_graph())
+                flowchart.append(f'so{self.id} --> sub{hierarchy[0].id}')
+            elif len(hierarchy) == 1:
+                flowchart.append(hierarchy[0]._get_flowchart_click())
+                flowchart.append(f'so{self.id} --> {hierarchy[0]._get_flowchart_element()}')
+        return '\n'.join(flowchart)
+
+    def _get_flowchart_style(self):
+        return """
+        classDef bg-info-light fill:#17a2b880,color:black,stroke:unset;
+        classDef bg-success-light fill:#28a74580,color:black,stroke:unset;
+        classDef bg-warning-light fill:#ffac0080,color:black,stroke:unset;
+        classDef bg-danger-light fill:#dc354580,color:black,stroke:unset;
+        classDef bg-grey-light fill:#d3d3d380,color:black,stroke:unset;
+        """
+
+    def _get_flowchart_state_color(self):
+        return {
+            'draft': 'bg-info-light',
+            'sent': 'bg-info-light',
+            'sale': 'bg-success-light',
+            'done': 'bg-success-light',
+            'cancel': 'bg-danger-light',
+        }
+
+    def _get_flowchart_i18n(self):
+        return dict(self.env[self._name].fields_get(allfields=['state'])['state']['selection'])
+
+    def _get_flowchart_element(self, i18n=None, more_info=False):
+        self.ensure_one()
+        if not i18n:
+            i18n = self._get_flowchart_i18n()
+        state = self._get_flowchart_state_color().get(self.state)
+        if more_info:
+            return f'so{self.id}("{self.name} - {_(i18n[self.state])}\n{self.partner_id.display_name}"):::{state}'
+        return f'so{self.id}({self.name} - {_(i18n[self.state])}):::{state}'
+
+    def _get_flowchart_click(self):
+        self.ensure_one()
+        menu_id = self.env.ref('sale.sale_order_menu')
+        return f'click so{self.id} "/web#id={self.id}&model=sale.order&view_type=form&menu_id={menu_id.id}" _blank'
