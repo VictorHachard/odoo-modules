5b5ebc64aa38b052f069a104021183d2a1d25f08
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2024-06-01 20:57:56 +0200

    [stock_flowchart] Add stock_flowchart

---
+++ stock_flowchart/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Stock Flowchart",
+    "summary": "Technical module that adds a generic flowchart to stock for use by other modules.",
+    "description": "Technical module that adds a generic flowchart to stock for use by other modules.",
+    "category": "Technical",
+    "version": "0.0.1",
+    "license": "AGPL-3",
+    "author": "Victor",
+    "price": 0,
+    "currency": 'EUR',
+    "depends": ["stock", 'web_widget_mermaid_field'],
+    "data": [
+    ],
+    "installable": True,
+    'application': False,
+}
