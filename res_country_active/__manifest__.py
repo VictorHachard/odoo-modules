d9c4fe0e324625e352c8ad48be481f0bfd4f558a
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2024-08-11 08:53:19 +0200

    [res_country_active] Add res_country_active

---
+++ res_country_active/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Country Active",
+    "summary": "This module adds an active field to the country model.",
+    "description": "This module adds an active field to the country model.",
+    "category": 'Technical',
+    "version": "0.0.1",
+    "license": "AGPL-3",
+    "author": "Victor",
+    "price": 0,
+    "currency": 'EUR',
+    "depends": ["base"],
+    "data": [
+        "views/res_country.xml",
+    ],
+    "installable": True,
+    'application': True,
+}
